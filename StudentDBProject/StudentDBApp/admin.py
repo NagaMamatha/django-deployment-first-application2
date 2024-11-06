df7da6a7636929f6d9e07048fdc50e7dcf13b69d
NagaMamatha <yenamalanagamamatha@gmail.com>
2024-11-06 19:35:39 +0530

    firstcommit

---
+++ StudentDBProject/StudentDBApp/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from StudentDBApp.models import Student,Student2
+
+#Register your models here.
+class StudentAdmin(admin.ModelAdmin):
+    list_display=['name','marks']
+admin.site.register(Student,StudentAdmin);
+
+
+
+class Student2Admin(admin.ModelAdmin):
+    list_display=['rollno', 'name', 'dob','marks','email', 'phonenumber','address']
+
+admin.site.register(Student2,Student2Admin);  #register takes only 2-args at a time
+
+
